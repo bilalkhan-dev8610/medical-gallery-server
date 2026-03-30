7b812ecaec475b1c37b86ca9ed8671e7a4c8b649
Bilal <bilalkhan88130@gmail.com>
2026-03-30 12:44:40 +0530

    fix: use render PORT env variable

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const fileRoutes = require("./routes/files");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors({
