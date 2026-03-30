57f20265b45549250960156fecbc323f485e5704
Bilal <bilalkhan88130@gmail.com>
2026-03-30 12:10:08 +0530

    fix: add netlify url to cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000" ,"https://your-app-name.netlify.app",],
+  origin: ["http://localhost:5173", "http://localhost:3000" ,"https://gallery-portal01.netlify.app",],
   
   methods: ["GET", "POST", "DELETE"],
 }));
